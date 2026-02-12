c99b2e8e83b2c84599bd9bab1e5c1eedbe2a055c
vijayviruzthalapathy-cmd <vijayviruzthalapathy@gmail.com>
2026-02-12 23:15:40 +0530

    Add functions to manage menu and cart visibility

---
+++ script.js
@@ -0,0 +1,15 @@
+function openMenu() {
+  document.getElementById("sideMenu").classList.add("active");
+}
+
+function closeMenu() {
+  document.getElementById("sideMenu").classList.remove("active");
+}
+
+function openCart() {
+  document.getElementById("cartPanel").classList.add("active");
+}
+
+function closeCart() {
+  document.getElementById("cartPanel").classList.remove("active");
+}
